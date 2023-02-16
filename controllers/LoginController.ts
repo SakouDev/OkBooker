5e55185acdd5325eb0cad6c8977e558e324e4130
SAKUTO-PORTABLE\LucVi <vigneronlucweb@gmail.com>
2023-02-16 09:39:41 +0100

    commit

--- controllers/LoginController.ts
+++ controllers/LoginController.ts
@@ -2,27 +2,53 @@
 import axios from "axios"
 import { Request, Response } from "express"
 
-const login = async (req:Request, res: Response) => {
+const loginAdmin = async (req:Request, res: Response) => {
     const result = await axios.get('http://141.94.247.187:3000/api/v1/list').then((response) => {
         return response.data
     })
     const {name, code} = req.body
-    let bool = false
 
-    result.forEach((element:any) => {
-        if(element.name === name && element.code === code){
-            bool = true
+    const data = result.filter((item:any) => {
+        return item.code === code && item.name === name
+    })
+
+    console.log(data)
+    
+    if(data.length > 0) {
+        if(data[0].admin) {
+            res.status(200).json({message : "Logged In", data})
+        }else {
+            res.status(401).json({message : "Wrong Credentials"})
         }
-    });
-
-    if(bool){
-        res.status(200).json({message : "Logged In"})
-    }else{
+    } else {
         res.status(401).json({message : "Wrong Credentials"})
     }
+    
+
+}
+
+const loginQR = async (req:Request, res: Response) => {
+    const result = await axios.get('http://141.94.247.187:3000/api/v1/list').then((response) => {
+        return response.data
+    })
+    const {name, code} = req.body
+
+    const data = result.filter((item:any) => {
+        return item.code === code
+    })
+
+    console.log(data)
+    
+    if(data.length > 0) {
+        res.status(200).json({message : "Logged In", data})
+    } else {
+        res.status(401).json({message : "Wrong Credentials"})
+    }
+    
 
 }
 
 export default {
-    login
+    loginAdmin,
+    loginQR
 }
--- routes/LoginRoutes.ts
+++ routes/LoginRoutes.ts
@@ -26,7 +26,26 @@ const router = Router();
   *        200:
   *          description: Returns a mysterious string.
   */
-router.post('/', LoginController.login)
+router.post('/', LoginController.loginAdmin)
+
+/**
+  * @openapi
+  * /api/login/qr:
+  *  post:
+  *      tags: [Login]
+  *      description: Login
+  *      parameters:
+  *       - name: JSON
+  *         in: body
+  *         required: true
+  *         type: object
+  *         default: {"emprunt": "2023-02-01T00:00:00.000Z", "rendu": null, "user": 2 }
+  *      responses:
+  *        200:
+  *          description: Returns a mysterious string.
+  */
+router.post('/qr', LoginController.loginQR)
+
 
 
 export default router
